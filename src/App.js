6faf08ed863914cc67bb45df6455457f1731457b
wilembergson <wilembergson@yahoo.com>
2021-07-14 10:47:21 -0300

    Projeto criado

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import 'bootswatch/dist/flatly/bootstrap.css'
+
+function App() {
+  return (
+    <div>
+      <button type="button" className="btn btn-primary">teste</button>
+    </div>
+  );
+}
+
+export default App;
