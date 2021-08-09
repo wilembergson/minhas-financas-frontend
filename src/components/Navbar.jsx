f6438e7132cb6b0cb5b77a6a761695493112dc33
wilembergson <wilembergson@yahoo.com>
2021-08-09 11:59:27 -0300

    Logo no navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,23 @@ import React from 'react'
 import './Navbar.css'
 import NavbarItem from './NavbarItem'
 import { AuthConsumer } from '../main/ProvedorAutenticacao'
+import { AuthContext } from '../main/ProvedorAutenticacao'
+import Logo from '../img/logo_nav.png'
 
 function Navbar(props){
+
+  const contexto = React.useContext(AuthContext)
 
     return(
     <div className="navbar navbar-expand-lg fixed-top navbar-dark">
       <div className="container">
-            <a href="/home" className="navbar-brand">Minhas Finanças</a>
+            <a href={contexto.isAutenticado ? "#/home" : "/"} className="navbar-brand"><img className="logonav" src={Logo}/></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
                 <ul className="navbar-nav">
-                    <NavbarItem render={props.isUsuarioAutenticado} href="#/home" label="Home"/>
+                    <NavbarItem render={props.isUsuarioAutenticado} href="#/home" exact label="Home"/>
                     <NavbarItem render={props.isUsuarioAutenticado} href="#/cadastroUsuario" label="Usuários"/>
                     <NavbarItem render={props.isUsuarioAutenticado} href="#/consultaLancamentos" label="Lançamentos"/>
                     <NavbarItem render={props.isUsuarioAutenticado} onClick={props.deslogar} href="/" label="Sair"/>
